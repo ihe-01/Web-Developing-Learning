ea627ced611773115b2080aa299d2db5278d75f7
Ivy He <ivy.he01.college@gmail.com>
2025-06-28 17:52:20 -0500

    done with js toturial?

--- playwithjs.js
+++ playwithjs.js
@@ -1,4 +1,4 @@
-function testing(val) {
+function caseExample1(val) {
     var answer = "";
     switch(val) {
         case 1:
@@ -18,7 +18,7 @@ function testing(val) {
 }
 
 var count = 0;
-function cards (card) {
+function caseExample2(card) {
     switch (card) {
         case 2:
         case 3:
@@ -58,3 +58,43 @@ for (i=0; i < array.length; i++) {
         total *= [i][k] 
     }
 }
+
+function randomWithinRange(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function ternaryExample1(num) {
+    return num > 0 ? "positive" : "negative";
+} 
+/*
+    With ternary operators, the condition comes first.
+    ? (question mark) represents what happens if the condition is true.
+    : (colon) represents what happens if the condition is false
+    below is an examlpe of a nested ternary operator.
+*/
+function ternaryExample2(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+// Arrow functions will use => after the parameters and there will be no need for the curly brackets
+const arrowFunction = (array1, array2) => array1.concat(array2);
+
+//Destructuring objects
+const temp = {today: 77.5, tomorrow: 79};
+const {tomorrow : tempTrmw} = temp;
+return tempTrmw;
+
+const localTemp = {today: {min: 72, max: 84}, tomorrow: {min: 73, max: 85}};
+const {today : {min : tempToday }} = localTemp;
+/*
+    This second example destructures twice.
+    for nested objects, however many times you nest, 
+    is however many times you need to destructure to get the value.
+ */
+
+//Destructuring arrays 
+const [x, y, z] = [1, 2, 3, 4, 5, 6]
+[x, y] = [y, x];
+// for each variable, one item will go in it, and it will go in order.
+// for arrays, it can also be used to switch the values of two variables.
+
+//constant literals 
